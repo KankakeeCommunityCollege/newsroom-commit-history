1a72ea8262131c16a13aceba4de9eb75d712cf4e
wdzajicek <tig.welder4@gmail.com>
2018-07-19 21:19:20 -0500

    Fix IE 11 display issues of latest-news-widget cirlce images...
    Display: Flex was causing an issue in IE to border-radius of circle images:
    
    Create IE specific JS and Classes to remove display: flex and add other styles to accomodate IE.

---
+++ assets/js/ieOnly.js
@@ -0,0 +1,25 @@
+function GetIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+    // If IE, return version number.
+  if (Idx > 0) {
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+  }
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./)) {
+    return 11;
+  }
+  else {
+    return 0; //It is not IE
+  }
+}
+
+if (GetIEVersion() > 10) {
+  console.log("This is IE " + GetIEVersion());
+  $('[id*="jsTeaseLink"]').addClass('ie-only__no-flex');
+  $('.latest-news-widget__img-mod--crop').addClass('ie-only__float-right');
+  $('.latest-news-widget__latest__tease__hgroup').addClass('ie-only__hgroup');
+  //var howWide = $(window).innerWidth();
+  //console.log(howWide);
+}
