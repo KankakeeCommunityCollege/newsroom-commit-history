e1f7b35d76157eeb677bb98cc4194bc6ac520888
wdzajicek <tig.welder4@gmail.com>
2018-07-20 07:46:48 -0500

    Cleanup code IE-11 fix for latest-news-widget...
    (Second "latest-news-widget" circle icon was displaying as an oval in IE11:
    Fix circle icon display on "News" page.

--- assets/js/ieOnly.js
+++ assets/js/ieOnly.js
@@ -18,8 +18,11 @@ function GetIEVersion() {
 if (GetIEVersion() > 10) {
   console.log("This is IE " + GetIEVersion());
   $('[id*="jsTeaseLink"]').addClass('ie-only__no-flex');
-  $('.latest-news-widget__img-mod--crop').addClass('ie-only__float-right');
+  $('.latest-news-widget__latest__tease__img-mod--default').addClass('ie-only__float-right');
   $('.latest-news-widget__latest__tease__hgroup').addClass('ie-only__hgroup');
   //var howWide = $(window).innerWidth();
   //console.log(howWide);
 }
+
+var windowSize = $(window).innerWidth();
+console.log(windowSize);
