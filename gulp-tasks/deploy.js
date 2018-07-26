3ef6c286d425c8ec2a6a503748e251b57e3c896b
wdzajicek <tig.welder4@gmail.com>
2018-07-26 15:16:34 -0500

    Update deploy.

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -13,35 +13,35 @@ gulp.task('newerFTP', function() {
   var remotePath = '/';
   var conn = ftp.create({
     host: 'Web03.kcc.edu',
-  //user: args.user,
+    //user: args.user,
     user: args.user,
     //password: args.password,
     password: args.password,
     log: gutil.log
   });
   var globs = [
-          '_site/*.*',
-          '_site/**/*.*',
-          '_site/**/**/*.*',
-          '_site/**/**/**/*.*',
-          '_site/index.html'
-      ];
+    '_site/*.*',
+    '_site/**/*.*',
+    '_site/**/**/*.*',
+    '_site/**/**/**/*.*',
+    '_site/index.html'
+  ];
 
-      // using base = '.' will transfer everything to /public_html correctly
-      // turn off buffering in gulp.src for best performance
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
 
-      return gulp.src( globs, { base: './_site/', buffer: false } )
-          .pipe( conn.newer( remotePath ) ) // only upload newer files
-          .pipe( conn.dest( remotePath ) );
-  } );
+  return gulp.src( globs, { base: './_site/', buffer: false } )
+    .pipe( conn.newer( remotePath ) ) // only upload newer files
+    .pipe( conn.dest( remotePath ) );
+} );
 
-  // Remove dist/scripts directory
-  gulp.task( 'cleanDir', function () {
-      var conn = ftp.create({
-          host: 'Web03.kcc.edu',
-          user: args.user,
-          password: args.password,
-          log: gutil.log
-    });
-    return conn.clean( ['/**', '/*.*', '/**/*.*', '/**/**/*.*', '/**/**/**/*.*'], './_site/', { base: './_site/' });
+// Remove dist/scripts directory
+gulp.task( 'cleanDir', function () {
+  var conn = ftp.create({
+    host: 'Web03.kcc.edu',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
   });
+  return conn.clean( ['/**', '/*.*', '/**/*.*', '/**/**/*.*', '/**/**/**/*.*'], './_site/', { base: '.' });
+});
