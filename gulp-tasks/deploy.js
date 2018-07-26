8748cc1bc0b21003f1731419d78b4b3a319b21fc
wdzajicek <tig.welder4@gmail.com>
2018-07-26 18:05:27 -0500

    Test deploy.

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -5,26 +5,27 @@ var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
-var remotePath = '/';
-var conn = ftp.create({
-  host: 'Web03.kcc.edu',
-  user: args.user,
-  password: args.password,
-  log: gutil.log
-});
-var globs = [
-  '_site/*.*',
-  '_site/**/*.*',
-  '_site/**/**/*.*',
-  '_site/**/**/**/*.*',
-  '_site/index.html'
-];
-
 gulp.task('deploy', function(done) {
-  sequence( 'cleanFTP','newerFTP', done);
+  sequence( 'cleanFTP', 'newerFTP', done);
 });
 
 gulp.task('cleanFTP', function() {
+  var remotePath = '/';
+  var conn = ftp.create({
+    host: 'Web03.kcc.edu',
+    //user: args.user,
+    user: args.user,
+    //password: args.password,
+    password: args.password,
+    log: gutil.log
+  });
+  var globs = [
+    '_site/*.*',
+    '_site/**/*.*',
+    '_site/**/**/*.*',
+    '_site/**/**/**/*.*',
+    '_site/index.html'
+  ];
     // using base = '.' will transfer everything to /public_html correctly
     // turn off buffering in gulp.src for best performance
   return gulp.src( globs, { base: './_site/', buffer: false } )
@@ -33,8 +34,26 @@ gulp.task('cleanFTP', function() {
 } );
 
 gulp.task('newerFTP', function() {
+  var remotePath = '/';
+  var conn = ftp.create({
+    host: 'Web03.kcc.edu',
+    //user: args.user,
+    user: args.user,
+    //password: args.password,
+    password: args.password,
+    log: gutil.log
+  });
+  var globs = [
+    '_site/*.*',
+    '_site/**/*.*',
+    '_site/**/**/*.*',
+    '_site/**/**/**/*.*',
+    '_site/index.html'
+  ];
+
     // using base = '.' will transfer everything to /public_html correctly
     // turn off buffering in gulp.src for best performance
+
   return gulp.src( globs, { base: './_site/', buffer: false } )
     .pipe( conn.newer( remotePath ) ) // only upload newer files
     .pipe( conn.dest( remotePath ) );
