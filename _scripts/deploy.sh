529d1e8cff051b48c9c648bd141d07dead3975a2
wdzajicek <tig.welder4@gmail.com>
2018-07-27 08:26:54 -0500

    Seperate the 2 tasks within Gulp deploy task. Add a delay between cleaning FTP and...
    putting the site back up.  Travis will run cleanFTP and wait 10 seconds to start putting the site back up.
    
    Reason:
    Microsoft IIS server is giving a 550 error saying one of the files being FTP'd is being used by another process on the server end.
    The hope is to give the server a pause before throwing everything back onto it.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-gulp deploy --user $FTP_USER --password $FTP_PASSWORD
+echo 'Cleaning up Server Site'
+
+sleep 1
+
+gulp cleanFTP --user $FTP_USER --password $FTP_PASSWORD
+
+sleep 10
+
+echo 'Sending Files to Server via FTP'
+
+sleep 5
+
+gulp newerFTP --user $FTP_USER --password $FTP_PASSWORD
